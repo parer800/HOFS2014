736b4818b083465020c1347385ecff1089c63a21
simbe109 <simbe109@student.liu.se>
2014-05-24 15:43:20 +0200

    Simba

--- summer-app/public/controllers/spotifyDataController.js
+++ summer-app/public/controllers/spotifyDataController.js
@@ -1,36 +1,62 @@
 angular.module( 'spotifyDataController', [] ).controller('SpotifyDataCtrl', function ($scope, $http,weatherData){
 
-  $scope.berta = "Simon prooovar lite...";
+
 
   $scope.search = function(data){
 
-  	var searchdata = 'http://ws.spotify.com/search/1/album.json?q=' + data; // Data = sökord
+  var weather = weatherData.getProperty();  // Get key word to search for album with ...Get the key from weather
+  var albums;
+  var result = []; // result to send back..
 
-  	 $http.get(searchdata)
-  		.success(function(data) {
+  var searchdata = 'http://ws.spotify.com/search/1/album.json?q=' + data; // Data = sökord
 
-  			// Plockar ut alla album
-    	$scope.spotifyData = data.albums;
-    	console.log(data.albums);
+	 $http.get(searchdata)
+		.success(function(data) {
 
-  	})
-	  .error(function(data) {
-	    console.log('Error: ' + data);
-	  });
+			// Plockar ut alla album
+  	albums = data.albums;
 
-	  var temp = weatherData.getProperty();
+    var nrOfalbums;
+    // Select the three first albums... if three exists
+    if(albums.length < 3){
+        nrOfalbums = albums.length;
+    }
+    else{
+      nrOfalbums = 3;
+    }
 
-	  console.log("tst service: ",temp);
-	  // Har vi tio album
-	 /* if($scope.spotifyData.length > 10)
-	  {
-	  	var hej = 10;
-	  }
-	  else 
-	  {
-	  	var hej = $scope.spotifyData.length
-	  }
-*/
+    var i;
+    var albumID;
+    for(i=0;i<nrOfalbums;i++)
+    {
+       albumID = albums[i].href;
+
+      searchdata = 'http://ws.spotify.com/lookup/1/.json?uri='+albumID+'&extras=trackdetail';
+      console.log("Next query..");
+
+      $http.get(searchdata)
+      .success(function(data1) {
+
+        var j;
+        for(j=0;j<3;j++){
+
+            result.push({"song":data1.album.tracks[j].name, "artist": data1.album.tracks[j].artists[0].name , "duration": data1.album.tracks[j].length});
+        } 
+        console.log("Result: ",result);
+
+      }).error(function(data) {
+        console.log('Error: ' + data1);
+      });
+
+    }  
+	})
+  .error(function(data) {
+    console.log('Error: ' + data);
+  });
+
+
+
+
 
   };
 
